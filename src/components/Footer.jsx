526057a8dc99932a8b9e8f06b8b7bbdd89cc32cd
Ritik17 <ritik.singh@binarynumbers.io>
2021-03-30 21:43:31 +0530

    added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React from "react";
 
 export default function Footer() {
-    return (
-        <>
-        <footer>
-            <p className="text-center">This is footer</p>
-            </footer></>
-    )
+  return (
+    <>
+      <footer>
+        <div className="container">
+          <section className="row">
+            <article className="col-md-9 mx-auto">
+              <section className="d-flex justify-content-between">
+                {["Feed", "Post", "Share"].map((prefix) => (
+                  <article className="footer-links">
+                    {["back", "up", "bush", "all"].map((suffix) => (
+                      <p>
+                        {prefix}
+                        {suffix}
+                      </p>
+                    ))}
+                  </article>
+                ))}
+              </section>
+            </article>
+          </section>
+        </div>
+      </footer>
+    </>
+  );
 }
--- src/components/WriteArticle.jsx
+++ src/components/WriteArticle.jsx
@@ -1,40 +1,56 @@
-import React, {useEffect, useRef} from 'react'
-import {useHistory} from 'react-router-dom'
+import React, { useEffect, useRef } from "react";
+import { useHistory } from "react-router-dom";
 
 export default function WriteArticle() {
-    const history = useHistory();
-    const refTitle = useRef();
-    const refOpinion = useRef();
+  const history = useHistory();
+  const refTitle = useRef();
+  const refOpinion = useRef();
 
-    function addArticle() {
-        let post = {title: refTitle.current.value, opinion: refOpinion.current.value};
-        if(localStorage.posts){
-            const parsedArray = JSON.parse(localStorage.posts);
-            parsedArray.push(post);
-            localStorage.posts = JSON.stringify(parsedArray)
-        }
-        else{
-            let postsArray=[] 
-            postsArray.push(post);
-            localStorage.posts = JSON.stringify(postsArray);
-        }
-        history.push("/article");
+  function addArticle() {
+    let post = {
+      title: refTitle.current.value,
+      opinion: refOpinion.current.value,
+    };
+    if (localStorage.posts) {
+      const parsedArray = JSON.parse(localStorage.posts);
+      parsedArray.push(post);
+      localStorage.posts = JSON.stringify(parsedArray);
+    } else {
+      let postsArray = [];
+      postsArray.push(post);
+      localStorage.posts = JSON.stringify(postsArray);
     }
-    return (
-        <>
-            <div className="container header-space">
-            <div class="form-floating mb-3">
-  <input type="email" class="form-control bg-dark text-light" id="title" ref={refTitle} placeholder="Type in the title here" autofocus/>
-  <label for="title">Title</label>
-</div>
-            <div class="form-floating">
-  <textarea class="form-control bg-dark text-light mb-3" placeholder="Leave a comment here" ref={refOpinion} id="opinion"></textarea>
-  <label for="opinion">Opinion</label>
-</div>
-            <article className="col-xl-3 col-md-4 col-sm-6 mx-auto">
-            <button className="btn-def w-100" onClick={addArticle}>Submit</button>
-            </article>
-            </div>
-        </>
-    )
+    history.push("/article");
+  }
+  return (
+    <>
+      <div className="container header-space">
+        <div class="form-floating mb-3">
+          <input
+            type="email"
+            class="form-control bg-dark text-light"
+            id="title"
+            ref={refTitle}
+            placeholder="Type in the title here"
+            autofocus
+          />
+          <label for="title">Title</label>
+        </div>
+        <div class="form-floating">
+          <textarea
+            class="form-control bg-dark text-light mb-3"
+            placeholder="Leave a comment here"
+            ref={refOpinion}
+            id="opinion"
+          ></textarea>
+          <label for="opinion">Opinion</label>
+        </div>
+        <article className="col-xl-3 col-md-4 col-sm-6 mx-auto">
+          <button className="btn-def w-100" onClick={addArticle}>
+            Submit
+          </button>
+        </article>
+      </div>
+    </>
+  );
 }
